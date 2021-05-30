d50439b51e6d12b13427baa9857945a246603163
fcotabar <fcotabar@gmail.com>
2021-05-30 15:53:05 -0500

    Initial State

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>This is my Header</h1>
       </header>
+      <footer className="App-footer">
+        <h3>This is my footer</h3>
+      </footer>
     </div>
   );
 }
